728da1a09dfcc5d462787adcccddb015a81ce135
Josef <josef.amstler@gmail.com>
2024-08-27 10:11:20 +0200

    Initial commit

---
+++ ubuntu_coding_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bach
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install lts
+
+# sdkman
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+# pnpm
+npm install -g pnpm
+
+# vscode
+sudo snap install --classic code
+
+mv settings.json ~/.config/Code/User
